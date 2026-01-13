bcce2b8a1fbfa073fde7ca7dd7615d2884bd0ea7
Meir-Gerby <meirg2001@gmail.com>
2026-01-13 12:27:08 +0200

    add singelton class

--- service_b/storege.py
+++ service_b/storege.py
@@ -1,2 +1,8 @@
 import redis 
 
+class SingletonClass(object):
+  def __new__(cls):
+    if not hasattr(cls, 'instance'):
+      cls.instance = super(SingletonClass, cls).__new__(cls)
+    return cls.instance 
+  
