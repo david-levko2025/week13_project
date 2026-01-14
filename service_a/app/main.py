04cabf43672117087f2e1d553e43800d185b4365
david <mdavidl@gmail.com>
2026-01-14 12:08:32 +0200

    pull

--- service_a/app/main.py
+++ service_a/app/main.py
@@ -8,4 +8,4 @@ app.include_router(routes.router,prefix="/ip",tags=["IP"])
 
 
 if __name__ == "__main__":
-    uvicorn.run(app,host="localhost",port=8000)
+    uvicorn.run(app,host="localhost",port=8080)
